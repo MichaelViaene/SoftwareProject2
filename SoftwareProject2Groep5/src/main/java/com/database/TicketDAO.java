272b46740eb13e9fb6135f7111f0bf9383a2324f
BuildTools <unconfigured@null.spigotmc.org>
2016-10-28 17:18:33 +0200

    TicketDAO: LinkedList<Ticket> toegevoegd die alle tickets returned.

--- SoftwareProject2Groep5/src/main/java/com/database/TicketDAO.java
+++ SoftwareProject2Groep5/src/main/java/com/database/TicketDAO.java
@@ -3,7 +3,9 @@ import com.model.Ticket;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.Timestamp;
+import java.sql.ResultSet;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Vik Mortier on 28/10/2016.
@@ -49,5 +51,51 @@ public class TicketDAO {
             return false;
         }
         return true;
+    }
+
+    public List<Ticket> readTickets (){
+
+        List<Ticket> tickets = new LinkedList<>();
+
+        try {
+            Connection con = Database.getConnection();
+            if (con == null) {
+                Database.openDatabase();
+                con = Database.getConnection();
+            }
+
+            if (con != null) {
+                String query = "SELECT * FROM Ticket";
+                PreparedStatement preparedStatement = con.prepareStatement(query);
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                while (resultSet.next()) {
+
+                    Ticket ticket = new Ticket();
+
+                    ticket.setTicket_id(resultSet.getInt("ticket_id"));
+                    ticket.setVertrekStation(resultSet.getString("vertrek"));
+                    ticket.setEindStation(resultSet.getString("aankomst"));
+                    ticket.setDatumAankoop(resultSet.getTimestamp("datum_aankoop").toLocalDateTime());
+                    ticket.setDatumHeen(resultSet.getDate("datum_heen").toLocalDate());
+                    ticket.setDatumTerug(resultSet.getDate("datum_terug").toLocalDate());
+                    ticket.setKlasse(resultSet.getInt("class"));
+                    ticket.setPrijs(resultSet.getDouble("prijs"));
+                    ticket.setType(resultSet.getInt("type"));
+
+                    /* resultSet.getInt("medewerker_id");
+                       nog niet geimplementeerd.
+                     */
+
+                    tickets.add(ticket);
+                }
+                resultSet.close();
+                preparedStatement.close();
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        return tickets;
     }
 }

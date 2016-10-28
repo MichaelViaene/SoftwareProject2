e2d86f4c659098c39bc90da84d7fb609f45f8917
Jordan Vander Elst <jordan.vander.elst@student.ehb.be>
2016-10-29 00:06:17 +0200

    LoginDAO resultset en preparedstatement sluiten in finally block

--- SoftwareProject2Groep5/src/main/java/com/database/LoginDAO.java
+++ SoftwareProject2Groep5/src/main/java/com/database/LoginDAO.java
@@ -1,9 +1,6 @@
 package com.database;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 import com.model.Login;
 import com.mysql.jdbc.exceptions.jdbc4.CommunicationsException;
@@ -12,6 +9,8 @@ public class LoginDAO {
 
 	public Login getLoginByUsername(String username) {
 		Login login = new Login();
+		ResultSet resultSet = null;
+		PreparedStatement preparedStatement = null;
 		try {
 			Connection con = Database.getConnection();
 			if (con == null) {
@@ -21,9 +20,9 @@ public class LoginDAO {
 
 			if (con != null) {
 				String query = "SELECT * FROM Login WHERE username=?";
-				PreparedStatement preparedStatement = con.prepareStatement(query);
+				preparedStatement = con.prepareStatement(query);
 				preparedStatement.setString(1, username);
-				ResultSet resultSet = preparedStatement.executeQuery();
+				resultSet = preparedStatement.executeQuery();
 				//login_id(int11),username(vchar45),passwoord(vchar45),bevoegdheid(int11),salt(vchar45)
 				while (resultSet.next()) {
 					login.setLogin_id(resultSet.getInt("login_id"));
@@ -32,16 +31,25 @@ public class LoginDAO {
 					login.setSalt(resultSet.getString("salt"));
 					login.setBevoegdheid(resultSet.getInt("bevoegdheid"));
 				}
-				resultSet.close();
-				preparedStatement.close();
 			}
 
 		} catch (Exception ex) {
 			System.out.println(ex);
+		} finally {
+			if (resultSet != null) {
+				try {
+					resultSet.close();
+				} catch (SQLException ex) {System.out.println(ex);}
+			}
+
+			if (preparedStatement != null) {
+				try {
+					preparedStatement.close();
+				} catch (SQLException ex) {System.out.println(ex);}
+			}
+
+
 		}
 		return login;
 	}
 }
-
-	
-

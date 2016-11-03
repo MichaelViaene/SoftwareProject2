ef0fd96b013a2719eeeaf2276adad01fc715c687
Jordan Vander Elst <jordan.vander.elst@student.ehb.be>
2016-11-03 14:37:43 +0100

    Layout aanpassing + event switchStations gelinkt aan button.

--- SoftwareProject2Groep5/src/main/java/com/ehbrail/wRouteInfoTabController.java
+++ SoftwareProject2Groep5/src/main/java/com/ehbrail/wRouteInfoTabController.java
@@ -1,7 +1,12 @@
 package com.ehbrail;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -10,13 +15,34 @@ import java.util.ResourceBundle;
  */
 public class wRouteInfoTabController implements Initializable {
 
+    @FXML private Button switchStationsButton;
+    @FXML private TextField vanField;
+    @FXML private TextField naarField;
 
-    
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
     }
+
+   @FXML private void switchStations(ActionEvent event) throws IOException {
+        try {
+            String temp = vanField.getText();
+            vanField.setText(naarField.getText());
+            naarField.setText(temp);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+
+
+
+
+
+
+
+
 
 }

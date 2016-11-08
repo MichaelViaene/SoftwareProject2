aa2137aa7db4a2d48b7d6c8be9b699d706457e40
Jordan Vander Elst <jordan.vander.elst@student.ehb.be>
2016-11-08 18:10:00 +0100

    RouteInformatie, met meer parameters. (Nog niet af)//TODO tijd kunnen ingeven

--- SoftwareProject2Groep5/src/main/java/com/ehbrail/wRouteInfoTabController.java
+++ SoftwareProject2Groep5/src/main/java/com/ehbrail/wRouteInfoTabController.java
@@ -1,5 +1,7 @@
 package com.ehbrail;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,13 +13,16 @@ import org.controlsfx.control.textfield.TextFields;
 
 import java.io.IOException;
 import java.net.URL;
-import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import com.model.Routes.*;
+
+import static com.ehbrail.ApiCalls.getExtendedIRailRoute;
 import static com.ehbrail.ApiCalls.getIRailRoute;
 import static com.ehbrail.WerknemerController.toLocalDateTime;
 
@@ -25,21 +30,51 @@ import static com.ehbrail.WerknemerController.toLocalDateTime;
  * Created by jorda on 28/10/2016.
  */
 public class wRouteInfoTabController implements Initializable {
+    ArrayList<String> list;
 
     @FXML private Button switchStationsButton;
     @FXML private Button planRouteButton;
     @FXML private TextField vanField;
     @FXML private TextField naarField;
-    @FXML TreeView<String> treeRoute;
-    @FXML Label errorLabel;
-    ArrayList<String> list;
+    @FXML private TreeView<String> treeRoute;
+    @FXML private Label errorLabel;
+    @FXML private DatePicker dateText;
+    @FXML private RadioButton aankomstRadio;
+    @FXML private RadioButton vertrekRadio;
+    @FXML private ToggleGroup timeSel;
+    @FXML private Spinner<Integer> hourSpinner;
 
+    private String radioText;
+
+//TODO Spinners uitwerken
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         list = LoginController.getList();
         TextFields.bindAutoCompletion(vanField,list);
         TextFields.bindAutoCompletion(naarField,list);
+        dateText.setValue(LocalDate.now());
+//------------------------------------------------//
+        timeSel.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            public void changed(ObservableValue<? extends Toggle> ov, Toggle old_toggle, Toggle new_toggle) {
+                if (timeSel.getSelectedToggle() != null) {
+                    RadioButton chk = (RadioButton)new_toggle.getToggleGroup().getSelectedToggle(); // Cast object to radio button
+                    //System.out.println("Selected Radio Button - "+chk.getText());
+                    if (chk.getText().equals("Vertrek")){radioText = "depart";}
+                    else radioText = "arrive";
+                }
+            }
+        });
+        aankomstRadio.setSelected(true);
+    }
+
+
+
+    private String dateFormat (LocalDate date){
+        DateTimeFormatter formatDate = DateTimeFormatter.ofPattern("ddMMyy");
+        String format_1=date.format(formatDate);
+        //System.out.println("Current date in format '': "+format_1);
+        return format_1;
     }
 
    @FXML private void switchStations(ActionEvent event) throws IOException {
@@ -55,7 +90,7 @@ public class wRouteInfoTabController implements Initializable {
 
 
     @FXML
-    private void onClickplanRoute(ActionEvent event) throws IOException {
+    private void onClickplanRoutea(ActionEvent event) throws IOException {
         if (vanField.getText().isEmpty() || naarField.getText().isEmpty()) errorLabel.setText("Gelieve een vertrek EN aankomst station mee te geven!");
         else {
             try {
@@ -79,6 +114,36 @@ public class wRouteInfoTabController implements Initializable {
             }
         }
     }
+
+
+@FXML
+private void onClickplanRoute(ActionEvent event) throws IOException {
+
+    if (vanField.getText().isEmpty() || naarField.getText().isEmpty()) errorLabel.setText("Gelieve een vertrek EN aankomst station mee te geven!");
+    else {
+        try {
+            Response response = getExtendedIRailRoute(vanField.getText(), naarField.getText(),dateFormat(dateText.getValue()),"0659",radioText);
+            System.out.println("Van:" +vanField.getText()+" Naar:"+ naarField.getText()+" date:"+dateFormat(dateText.getValue())+" Tijd:" +"0659"+" Type: "+radioText);
+            if (response.isSuccessful()) {
+                String jsonString = response.body().string();
+                ObjectMapper mapper = JsonFactory.create();
+                IrailRoute irailRoute = mapper.readValue(jsonString, IrailRoute.class);
+                TreeItem<String> rootItem = new TreeItem<String>("Routes");
+                createTreeItems(rootItem, irailRoute);
+                rootItem.setExpanded(true);
+                treeRoute.setRoot(rootItem);
+                errorLabel.setText(" ");
+            } else {
+                errorLabel.setText("Error, responsecode = " + response.networkResponse().code() + ", With message: " + response.networkResponse().message());
+                TreeItem<String> rootItem = new TreeItem<String>("Routes");
+                treeRoute.setRoot(rootItem);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
 
     private void createTreeItems(TreeItem<String> rootItem, IrailRoute irailRoute) throws IOException {
         TreeItem<String> rootItem1 = null;

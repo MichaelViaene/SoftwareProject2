f7ed1d4427290dd5fad2a47f954f888afdb9053b
Pieter <Pieter@10.3.101.5>
2016-11-22 11:06:09 +0100

    aanpassing ticketTabController

--- SoftwareProject2Groep5/src/main/java/com/ehbrail/wTicketTabController.java
+++ SoftwareProject2Groep5/src/main/java/com/ehbrail/wTicketTabController.java
@@ -1,24 +1,26 @@
 package com.ehbrail;
-import com.model.Login;
 import com.model.Ticket;
-import com.model.Werknemer;
 
 import java.io.IOException;
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.ResourceBundle;
 
-import javax.swing.ButtonGroup;
-import javax.swing.JOptionPane;
+import org.controlsfx.control.textfield.TextFields;
 
 import com.database.TicketDAO;
 
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Spinner;
@@ -34,9 +36,10 @@ import javafx.scene.control.Label;
  *
  */
 
-public class wTicketTabController {
-		ArrayList<String> list;
+public class wTicketTabController implements Initializable{
 
+	ArrayList<String> list;
+		
 	   @FXML private TextField vanField;
 	   @FXML private TextField naarField;
 	   @FXML private Button switchStationsButton;
@@ -48,7 +51,7 @@ public class wTicketTabController {
 	   @FXML private RadioButton heenVertrekRadio;
 	   @FXML private ToggleGroup heenVertrekAankomst;
 	   @FXML private RadioButton heenAankomstRadio;
-	   @FXML private Pane paneTerug;
+	   @FXML private Pane painTerug;
 	   @FXML private DatePicker datumTerugDatePicker;
 	   @FXML private RadioButton terugVertrekRadio;
 	   @FXML private ToggleGroup terugVertrekAankomst;
@@ -57,43 +60,48 @@ public class wTicketTabController {
 	   @FXML private ToggleGroup klasse;
 	   @FXML private RadioButton tweedeKlasseRadio;
 	   @FXML private Label terugLabel;
+	   
 
+	      
 	   @FXML
 	    void showPaneTerug(ActionEvent event) {
-	    	paneTerug.setVisible(true);
-	    	System.out.println("show pane terug");
-	    }
-	    
-	    @FXML
-	    void paneTerugWeg(ActionEvent event) {
-	    	paneTerug.setVisible(false);
-	    	System.out.println("pane terug weg");
+		   painTerug.setVisible(false);
 	    }
 
 	   @FXML
-	   void showPaneHeenTerug(ActionEvent event) {
-		   paneTerug.setVisible(true);
-		   System.out.println("show pane heen terug");
+	   void showPaneHeenTrug(ActionEvent event) {
+		   painTerug.setVisible(true);
 	   }
+       
+       @Override
+       public void initialize(URL location, ResourceBundle resources) {
+       list = LoginController.getList();
+        TextFields.bindAutoCompletion(vanField,list);
+        TextFields.bindAutoCompletion(naarField,list);
+       }
         
     @FXML
     void onClickKoopTicket(ActionEvent event) {
     	String vertrekStation = vanField.getText();
-    	String eindStation = naarField.getText();
-    	int type, klasse, heen, terug;
-    	LocalDate datumHeen = datumHeenDatePicker.getValue();
-    	LocalDate datumTerug = datumTerugDatePicker.getValue();
-    	LocalDateTime datumAankoop = LocalDateTime.now(ZoneId.of( "Europe/Brussels" ));
-    	
+   		String eindStation = naarField.getText();
+   		int type, klasse, heen, terug;
+   		LocalDate datumHeen = datumHeenDatePicker.getValue();
+   		LocalDate datumTerug = datumTerugDatePicker.getValue();
+   		LocalDateTime datumAankoop = LocalDateTime.now(ZoneId.of( "Europe/Brussels" ));
+   		LocalDate todayLocalDate = LocalDate.now(ZoneId.of( "Europe/Brussels" ));
+   		list = LoginController.getList();
     	/*
 		Voorbeeld localDate, LocaldateTime (present)
 		LocalDate todayLocalDate = LocalDate.now(ZoneId.of( "Europe/Brussels" ) );
 		LocalDateTime todayLocalDateTime = LocalDateTime.now(ZoneId.of( "Europe/Brussels" ));
-    	*/
-    	if (datumHeen == null || vertrekStation == "" || eindStation == ""){
-    		//doe niets + andere checks hier
-    		//eventuele popup
-    		JOptionPane.showMessageDialog(null, "Foutive gegevens!");
+    	*/   		
+    	if (controleerVanField() == false || controleerNaarField() == false || datumHeen == null || vertrekStation.isEmpty() || eindStation.isEmpty() || datumHeen.isBefore(todayLocalDate) || (heenEnTerugRadio.isSelected() && (datumTerug == null || datumTerug.isBefore(datumHeen)))){
+    		Alert alert = new Alert(AlertType.ERROR);
+    		alert.setTitle("Error Dialog");
+    		alert.setHeaderText(null);
+    		alert.setContentText("Foutieve gegevens!");
+
+    		alert.showAndWait();
     	} else {
     		if(heenRadio.isSelected()){
         		type = 0; //Indien heen reis
@@ -101,7 +109,7 @@ public class wTicketTabController {
         	} else{
         		type = 1; //Indien heen en terug reis
         	}
-        	if(eersteKlasseRadio.isSelected()){
+    		if(eersteKlasseRadio.isSelected()){
         		klasse = 1; //Indien eerste klasse
         	} else {
         		klasse = 2; // indien tweede klasse
@@ -117,20 +125,52 @@ public class wTicketTabController {
         		terug = 1;
         	}
         		
-        	Ticket ticket = new Ticket(vertrekStation,eindStation,1,klasse,type,1,datumAankoop,datumHeen,datumTerug);
+        	Ticket ticket = new Ticket(vertrekStation,eindStation,1,klasse,type,1,datumAankoop,datumHeen,datumTerug,WerknemerController.getLogin().getMedewerker_id());
         	
-        	//TicketDAO.writeTicket(ticket);
+        	TicketDAO.writeTicket(ticket);
+        	
+        	Alert alert = new Alert(AlertType.INFORMATION);
+        	alert.setTitle("Information Dialog");
+        	alert.setHeaderText(null);
+        	alert.setContentText("Ticket aangemaakt!");
+
+        	alert.showAndWait();
         	
         	System.out.println(ticket.toString());
+        	vanField.clear();
+        	naarField.clear();
+        	datumHeenDatePicker.setValue(null);
+        	datumTerugDatePicker.setValue(null);
+        	tweedeKlasseRadio.setSelected(true);
+        	heenVertrekRadio.setSelected(true);
+        	terugVertrekRadio.setSelected(true);
+        	heenRadio.setSelected(true);
+        	painTerug.setVisible(false);
+        	
     	}
+    	
+    }  
+    public boolean controleerVanField(){
+    	list = LoginController.getList();
+    	for (int i = 0; i < list.size();i++){
+    		if(list.get(i).equals(vanField.getText())){
+    			return true;
+    		}
+    	}
+    	
+    	return false;
     }
-
+    public boolean controleerNaarField(){
+    	list = LoginController.getList();
+    	for (int i = 0; i < list.size();i++){
+    		if(list.get(i).equals(naarField.getText())){
+    			return true;
+    		}
+    	}
+    	
+    	return false;
+    }
     
-    
-    
-    
-    
-
     @FXML private void switchStations(ActionEvent event) throws IOException {
         try {
             String temp = vanField.getText();
@@ -141,5 +181,6 @@ public class wTicketTabController {
             System.out.println(e);
         }
     }
+
 
 }

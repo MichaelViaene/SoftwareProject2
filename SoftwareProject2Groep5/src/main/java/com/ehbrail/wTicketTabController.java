a0d3d853d2e072fb8f2712f9ca8de903a53989b0
Jordan Vander Elst <jordan.vander.elst@student.ehb.be>
2016-11-24 03:02:11 +0100

    Functionaliteit: print Ticket toegevoegd. Contains [ new class JRPrintPreview.java (methode om voorbeeld pdf te zien in javaFX) changed wTicketTabController.java (methode createPDF added). resources[ nieuw: logo ehackb + grijze trein icon. TrainTicket.jasper en .jrxml changed pom.xml library voor JasperReports toegevoegd. ]]

--- SoftwareProject2Groep5/src/main/java/com/ehbrail/wTicketTabController.java
+++ SoftwareProject2Groep5/src/main/java/com/ehbrail/wTicketTabController.java
@@ -1,16 +1,30 @@
 package com.ehbrail;
 import com.model.Ticket;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.ResourceBundle;
+import java.util.*;
 
+import javafx.embed.swing.SwingNode;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.export.HtmlExporter;
+import net.sf.jasperreports.export.Exporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleHtmlExporterOutput;
+import net.sf.jasperreports.swing.JRViewer;
+import net.sf.jasperreports.view.JasperViewer;
 import org.controlsfx.control.textfield.TextFields;
 
 import com.database.TicketDAO;
@@ -39,7 +53,7 @@ import javafx.scene.control.Label;
 public class wTicketTabController implements Initializable{
 
 	ArrayList<String> list;
-		
+	private ResourceBundle language;
 	   @FXML private TextField vanField;
 	   @FXML private TextField naarField;
 	   @FXML private Button switchStationsButton;
@@ -60,7 +74,8 @@ public class wTicketTabController implements Initializable{
 	   @FXML private ToggleGroup klasse;
 	   @FXML private RadioButton tweedeKlasseRadio;
 	   @FXML private Label terugLabel;
-	   
+
+
 
 	      
 	   @FXML
@@ -75,6 +90,7 @@ public class wTicketTabController implements Initializable{
        
        @Override
        public void initialize(URL location, ResourceBundle resources) {
+		  language = resources;
        list = LoginController.getList();
         TextFields.bindAutoCompletion(vanField,list);
         TextFields.bindAutoCompletion(naarField,list);
@@ -128,14 +144,18 @@ public class wTicketTabController implements Initializable{
         	Ticket ticket = new Ticket(vertrekStation,eindStation,1,klasse,type,1,datumAankoop,datumHeen,datumTerug,WerknemerController.getLogin().getMedewerker_id());
         	
         	TicketDAO.writeTicket(ticket);
-        	
+
+			createPDF(ticket,language);
+
         	Alert alert = new Alert(AlertType.INFORMATION);
         	alert.setTitle("Information Dialog");
         	alert.setHeaderText(null);
         	alert.setContentText("Ticket aangemaakt!");
-
         	alert.showAndWait();
-        	
+
+
+
+
         	System.out.println(ticket.toString());
         	vanField.clear();
         	naarField.clear();
@@ -181,6 +201,96 @@ public class wTicketTabController implements Initializable{
             System.out.println(e);
         }
     }
+
+
+    private void createPDF(Ticket ticket, ResourceBundle language){
+    	try {
+			JasperPrint jasperPrint = null;
+			HashMap<String,Object> params = new HashMap<String,Object>();
+			//parameters voor Resourcebundle
+			String van = "Van";
+			String tot = "Tot";
+			String op = "Op";
+			String klasseLabel = "Klasse";
+
+			//Parameters van ticket
+			int ticket_id = 15;
+
+			if (ticket.getType() == 0) {
+				String typeTekst = "Geldig voor een enkele reis";
+				String type = "ENKEL";
+				params.put("typeTekst",typeTekst);
+				params.put("type",type);
+			}
+			if (ticket.getType() == 1){
+				String typeTekst = "Geldig voor een heen-en terug reis";
+				String type = "HEEN EN TERUG";
+				params.put("typeTekst",typeTekst);
+				params.put("type",type);
+			}
+
+			String klasseTekst = String.valueOf(ticket.getKlasse())+"e";
+			String vertrekStation = ticket.getVertrekStation();
+			String eindStation = ticket.getEindStation();
+			String prijs = (String.valueOf(ticket.getPrijs()));       //Zorg dat je de double naar String cast.
+			LocalDateTime datumAankoop = ticket.getDatumAankoop() ; //LocalDateTime.now(ZoneId.of( "Europe/Brussels" ));
+			LocalDate datumHeen = ticket.getDatumHeen()  ; // LocalDate.of(2016,12,16);
+
+			int datumAankoop_year = datumAankoop.getYear();
+			int datumAankoop_month = datumAankoop.getMonthValue();
+			int datumAankoop_day = datumAankoop.getDayOfMonth();
+			int datumAankoop_hour= datumAankoop.getHour();
+			int datumAankoop_minute = datumAankoop.getMinute();
+			int datumHeen_year = datumHeen.getYear();
+			int datumHeen_month = datumHeen.getMonthValue();
+			int datumHeen_day = datumHeen.getDayOfMonth();
+
+			//Resourcebundle
+			params.put("van",van);
+			params.put("tot",tot);
+			params.put("op",op);
+			params.put("klasseLabel",klasseLabel);
+			//Ticket params
+			params.put("ticket_id",ticket_id);
+			params.put("vertrekStation",vertrekStation);
+			params.put("eindStation",eindStation);
+			params.put("klasseTekst",klasseTekst);
+			params.put("prijs",prijs);
+			params.put("datumAankoop_year",datumAankoop_year);
+			params.put("datumAankoop_month",datumAankoop_month);
+			params.put("datumAankoop_day",datumAankoop_day);
+			params.put("datumAankoop_hour",datumAankoop_hour);
+			params.put("datumAankoop_minute",datumAankoop_minute);
+			params.put("datumHeen_year",datumHeen_year);
+			params.put("datumHeen_month",datumHeen_month);
+			params.put("datumHeen_day",datumHeen_day);
+
+			System.out.println("Generating PDF...");
+			//JasperReport jasperReport = JasperCompileManager.compileReport("src/main/resources/TrainTicket.jrxml");
+			//jasperPrint = JasperFillManager.fillReport(jasperReport,params, new JREmptyDataSource());
+			jasperPrint = JasperFillManager.fillReport("src/main/resources/TrainTicket.jasper",params,new JREmptyDataSource() );
+
+			/**
+			Stage stage = new Stage();
+			final SwingNode swingNode = new SwingNode();
+			swingNode.setContent(new JRViewer(jasperPrint));
+			StackPane pane = new StackPane();
+			pane.getChildren().add(swingNode);
+			stage.setScene(new Scene(pane, 800, 800));
+			stage.show();
+			 **/
+
+			JRPrintPreview printPreview = new JRPrintPreview(jasperPrint);
+			printPreview.show();
+/**
+			//JasperExportManager.exportReportToPdfFile(jasperPrint, "trainTicket.pdf");
+			JasperViewer jasperViewer = new JasperViewer(jasperPrint);
+			jasperViewer.setVisible(true);
+**/
+		}
+		catch (Exception e){e.printStackTrace();}
+	}
+
 
 
 }
